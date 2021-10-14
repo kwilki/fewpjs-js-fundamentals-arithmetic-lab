c93d506d568ca338408f7992b029bd879843a7ad
Kyle <kylewilkinson14@gmail.com>
2021-10-14 12:13:36 +1100

    completed tests

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2
 
-// Write your code here
+let random = Math.floor(Math.random() * 10)
+
+const num3 = 10
+const num4 = 6
+const mod = num3 - num4
+
+const numList = [1, 4, 6, 9, 20, 7, 2, 18, 13]
+const max = Math.max(...numList)
